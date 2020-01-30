f267d14ea86788228ca7c9c02d6c4ed16adb7e90
Joe Nelson <joe@begriffs.com>
2020-01-29 19:02:28 -0600

    Deadlock

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,49 @@
 use std::sync::Mutex;
 use std::sync::Arc;
 
-fn main() {
-    let balance = Arc::new(Mutex::new(0i64));
+use rand::prelude::*;
 
-    let thread = {
-        let balance = balance.clone();
-        std::thread::spawn(move || {
-            loop {
+const N_ACCTS: usize = 10;
+const N_THREADS: usize = 4;
 
-                match balance.lock() {
-                    Ok(mut balance) => {
-                        *balance += 1;
-                        println!("{}", balance);
-                    },
-                    Err(e) => eprintln!("NOOOO {:?}", e),
-                }
-            }
-        })
+fn disburse_random(accounts: &[Arc<Mutex<i64>>])
+{
+    let from = rand::thread_rng().gen_range(0, N_ACCTS);
+    let to = loop {
+        let to = rand::thread_rng().gen_range(0, N_ACCTS);
+        if to != from {
+            break to;
+        }
     };
 
-    loop {
-        match balance.lock() {
-            Ok(mut balance) => {
-                *balance -= 1;
-                println!("{}", balance);
-            },
-            Err(e) => eprintln!("NOOOO {:?}", e),
-        }
+    println!("Attempting {} -> {}", from, to);
+
+    let mut from_account = accounts[from].lock().unwrap();
+    let mut to_account = accounts[to].lock().unwrap();
+    let amt = rand::thread_rng().gen_range(0, *from_account);
+    *from_account -= amt;
+    *to_account += amt;
+
+    println!("{} -{}> {}", from, amt, to);
+}
+
+fn main() {
+    let accounts = vec![
+        Arc::new(Mutex::new(0i64)); N_ACCTS
+    ];
+    let mut threads = vec![];
+
+    for _ in 0..N_THREADS {
+        threads.push({
+            let accounts = accounts.clone();
+            std::thread::spawn(move || {
+                loop {
+                    disburse_random(&accounts);
+                }
+            })
+        });
     }
+    threads.into_iter().for_each(|t| {
+        t.join().unwrap();
+    });
 }
